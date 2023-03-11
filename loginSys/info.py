a1c0673fca6f5811ec5880f9ef3fd4b0dc2ec9e7
Prakashsoni12 <85999331+Prakashsoni12@users.noreply.github.com>
2023-03-11 14:43:57 +0530

    Security updated

--- loginSys/info.py
+++ loginSys/info.py
@@ -1,5 +1,5 @@
 EMAIL_USE_TLS= True
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'djangoeexample@gmail.com'
-EMAIL_HOST_PASSWORD = 'kmmrcueauellrhjp'
+EMAIL_HOST_USER = '@gmail.com'
+EMAIL_HOST_PASSWORD = ''
 EMAIL_PORT = 587
